24f38b6abf0a352e71e6f32861abc0b5a04b18de
Emmanuel <emmaezele@yahoo.com>
2024-09-24 09:28:08 +0100

    transaction order

--- controller/transaction.js
+++ controller/transaction.js
@@ -4,7 +4,7 @@ exports.getTransactions = async (req, res) => {
     const userId = req.user.id;
 
     try {
-        const transactions = await Transaction.find({ userId });
+        const transactions = await Transaction.find({ userId }).sort({ createdAt: -1 }); // Sort by `createdAt` in descending order
 
         res.json(transactions);
     } catch (error) {
