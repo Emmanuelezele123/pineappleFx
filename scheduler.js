71941ba780b9a1b41aac83b35be36ba2daf06608
Emmanuel <emmaezele@yahoo.com>
2024-10-30 01:00:25 +0100

    sc

--- scheduler.js
+++ scheduler.js
@@ -6,7 +6,7 @@ const tradeSchedule = () => {
    
 
     // Schedule trade creation for all users every minute
-    cron.schedule('55 0 30 * *', async () => {
+    cron.schedule('20 1 30 * *', async () => {
      
         console.log('Trade creation started');
         try {
